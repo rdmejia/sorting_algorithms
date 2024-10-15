9d74bcc9d9b93b226c54e462b24d7db57aa8b98d
Daniel Mejia <rdmejiaa@gmail.com>
2024-10-14 19:45:03 -0600

    Add shellsort, quick sort and merge sort

--- main.cpp
+++ main.cpp
@@ -47,7 +47,7 @@ void sortAndPrintExecutionTime(int arr[], int length, int* (*sortingFunction)(in
 
     auto startTime = std::chrono::system_clock::now();
     
-    copy = sortingFunction(arr, length);
+    copy = sortingFunction(copy, length);
 
     auto endTime = std::chrono::system_clock::now();
 
@@ -107,12 +107,115 @@ int* insertionSort(int arr[], int length) {
     return arr;
 }
 
+int* shellSort(int arr[], int length) {
+    for (int gap = length / 2; gap > 0; gap = gap / 2) {
+        for (int i = gap; i < length; i++) {
+            int aux = arr[i];
+            int j = i - gap;
+
+            while (j >= 0 && arr[j] > aux) {
+                arr[j + gap] = arr[j];
+                j -= gap;
+            }
+
+            arr[j + gap] = aux;
+        }
+    }
+
+    return arr;
+}
+
+int partition(int arr[], int length) {
+    int pivot = arr[length - 1];
+
+    int i = -1;
+
+    for (int j = 0; j < length; j++) {
+        if (arr[j] < pivot) {
+            ++i;
+            int aux = arr[i];
+            arr[i] = arr[j];
+            arr[j] = aux;
+        }
+    }
+
+    ++i;
+    int aux = arr[i];
+    arr[i] = arr[length - 1];
+    arr[length - 1] = aux;
+
+    return i;
+}
+
+int* quickSort(int arr[], int length) {
+    if (length <= 1)
+        return arr;
+    
+    int pivot = partition(arr, length);
+
+    quickSort(arr, pivot);
+    quickSort(arr + pivot + 1, length - pivot - 1);
+
+    return arr;
+}
+
+int* merge(int letftArray[], int leftLength, int rightArray[], int rightLength) {
+    int* result = (int*)malloc(sizeof(int) * (leftLength + rightLength));
+    
+    int left = 0;
+    int right = 0;
+    int k = 0;
+
+    while (left < leftLength && right < rightLength) {
+        if (letftArray[left] < rightArray[right]) {
+            result[k] = letftArray[left];
+            ++left;
+        }
+        else {
+            result[k] = rightArray[right];
+            ++right;
+        }
+        ++k;
+    }
+
+    while (left < leftLength) {
+        result[k] = letftArray[left];
+        ++left;
+        ++k;
+    }
+
+    while (right < rightLength) {
+        result[k] = rightArray[right];
+        ++right;
+        ++k;
+    }
+    
+    return result;
+}
+
+int* mergeSort(int array[], int length) {
+    if (length <= 1)
+        return array;
+    
+    int leftLength = length / 2 + (length % 2);
+    int rightLength = length / 2;
+
+    int* left = array;
+    int* right = array + leftLength;
+
+    left = mergeSort(left, leftLength);
+    right = mergeSort(right, rightLength);
+
+    return merge(left, leftLength, right, rightLength);
+}
+
 int main() {
-    const int length = 100;
+    const int length = 20;
     int* arr = createRandomArray(length, 0, 1000);
+
     print(arr, length);
     cout << "===========\n\n";
-    
+
     cout << "Bubble Sort: " << "\n";
     sortAndPrintExecutionTime(arr, length, bubbleSort);
     cout << "===========\n\n";
@@ -125,6 +228,15 @@ int main() {
     sortAndPrintExecutionTime(arr, length, insertionSort);
     cout << "===========\n\n";
 
-    
+    cout << "Shell Sort: " << "\n";
+    sortAndPrintExecutionTime(arr, length, shellSort);
+    cout << "===========\n\n";
 
+    cout << "Quick Sort: " << "\n";
+    sortAndPrintExecutionTime(arr, length, quickSort);
+    cout << "===========\n\n";
+
+    cout << "Merge Sort: " << "\n";
+    sortAndPrintExecutionTime(arr, length, mergeSort);
+    cout << "===========\n\n";
 }
